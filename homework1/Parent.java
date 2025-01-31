d7dbe5dcaede1a55b2f67f623b715daafd19c5fd
Baylen Romanello <baylen@waifu.club>
2025-01-31 12:31:28 -0500

    Add Parent class

--- homework1/Parent.java
+++ homework1/Parent.java
@@ -1,3 +1,11 @@
-public class Parent {
+//package cse274.homework1;
 
+public class Parent {
+  protected String name;
+  Parent(String name) {
+    name = "";
+  }
+  public void displayInfo() {
+    System.out.printf("Name: %s", name);
+  }
 }
