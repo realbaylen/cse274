80848ca4e8c221ac624d9a1c068f57d120e432e5
Baylen Romanello <baylen@waifu.club>
2025-02-05 13:21:18 -0500

    Added contains and indexOf methods

--- project1/src/Content.java
+++ project1/src/Content.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Content {
   private String content;
 
@@ -19,5 +21,25 @@ public class Content {
   // getContent getter method
   public String getContent() {
     return content;
+  }
+
+  // checks if key is in content string
+  public boolean contains(String key) {
+    if (key == null) {
+      return false;
+    } else {
+      return getName().contains(key);
+    }
+  }
+
+  // probably will need fixing
+  public int[] indexOf(String key) {
+    ArrayList<Integer> indexes = new ArrayList<Integer>();
+    for (int i = 0; i < getContent().length; i++) {
+      if (getContent().toLowerCase().charAt(i) == key) {
+        indexes.add(i);
+      }
+    }
+    return indexes.toArray();
   }
 }
