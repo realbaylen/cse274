c951295a7e646bc963e08237f39e381cefef5f17
Baylen Romanello <baylen@waifu.club>
2025-02-10 13:14:05 -0500

    Add getBytes method

--- project1/src/Joke.java
+++ project1/src/Joke.java
@@ -11,9 +11,29 @@ public class Joke extends Content implements Convertable, Comparable<Joke> {
   @Override
   public int compareTo(joke other) {
     if (this.getContent().length() == other.getContent().length()) {
-      return this.getContent().substring(0, 10).compareTo(other.getContent().substring(0, 10))
+      return this.getContent().substring(0, 10).compareTo(other.getContent().substring(0, 10));
     }
     return Integer.compare(this.getContent().length(),other.getContent().length());
 
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (!(obj instanceof Joke)) return false;
+    return this.compareTo((Joke) obj) == 0;
+  }
+
+  @Override
+  public byte[] getBytes(int byteSize) {
+    byteSize = Math.max(byteSize, 500);
+    byte[] result = new byte[byteSize];
+    byte[] categoryBytes = category.getBytes();
+    byte[] contentBytes = this.getContent().getBytes();
+    int categoryLength = Math.min(categoryBytes.length, 15);
+    System.arraycopy(categoryBytes, 0, result, 0, categoryLength);
+    int contentLength = Math.min(contentBytes.length, byteSize - 15);
+    System.arraycopy(contentBytes, 0, result, 15, contentLength);
+    return result;
   }
 }
