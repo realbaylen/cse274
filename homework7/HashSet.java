afe3e05186bce42716e51ac068ece8d4b2f30aad
Baylen Romanello <baylen@waifu.club>
2025-03-17 22:54:03 -0400

    Add remove, addAll

--- homework7/HashSet.java
+++ homework7/HashSet.java
@@ -1,50 +1,69 @@
-import java.util.LinkedList;
 import java.util.HashMap;
 import java.util.Iterator;
 
 public class HashSet<T extends Comparable<T>> implements Set<T> {
-    private HashMap<T, Object> map;
-    private static final Object DUMMY = new Object();
+  private HashMap<T, Object> map;
+  private static final Object DUMMY = new Object();
 
-    public HashSet() {
-        map = new HashMap<>();
+  public HashSet() {
+    map = new HashMap<>();
+  }
+
+  public HashSet(int capacity) {
+    map = new HashMap<>(capacity);
+  }
+
+  @Override
+  public boolean add(T value) {
+    if (map.containsKey(value)) {
+      return false;
     }
+    map.put(value, DUMMY);
+    return true;
+  }
 
-    public HashSet(int capacity) {
-        map = new HashMap<>(capacity);
+  @Override
+  public boolean remove(T value) {
+    if (map.containsKey(value)) {
+      map.remove(value);
+      return true;
     }
+    return false;
+  }
 
-    @Override
-    public boolean add(T value) {
-        if (map.containsKey(value)) {
-            return false;
+  @Override
+  public boolean contains(T value) {
+    return map.containsKey(value);
+  }
+
+  @Override
+  public int size() {
+    return map.size();
+  }
+
+  @Override
+  public void clear() {
+    map.clear();
+  }
+
+  @Override
+  public Iterator<T> iterator() {
+    return map.keySet().iterator();
+  }
+
+  @Override
+  public boolean isEmpty() {
+      return map.isEmpty();
+  }
+
+
+  public boolean addAll(Collection<? extends T> c) {
+    boolean modified = false;
+    for (T value : c) {
+        if (add(value)) {
+            modified = true;
         }
-        map.put(value, DUMMY);
-        return true;
     }
-
-    @Override
-    public void remove(T value) {
-        map.remove(value);
-    }
-
-    @Override
-    public boolean contains(T value) {
-        return map.containsKey(value);
-    }
-
-    @Override
-    public int size() {
-        return map.size();
-    }
-
-    @Override
-    public void clear() {
-        map.clear();
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return map.keySet().iterator();
-    }
+    return modified;
+}
 }
