ef5277f66597f9b87f8dca087489991abda7e005
Baylen Romanello <baylen@waifu.club>
2025-03-17 22:43:35 -0400

    add skeleton code generated by vs code

--- homework7/HashSet.java
+++ homework7/HashSet.java
@@ -1,1 +1,38 @@
-public class HashSet<T extends Comparable<T>> implements Set<T> {}
+public class HashSet<T extends Comparable<T>> implements Set<T> {
+    private HashMap<T, Object> map;
+    private static final Object DUMMY = new Object();
+
+    public HashSet() {
+        map = new HashMap<>();
+    }
+
+    @Override
+    public void add(T value) {
+        map.put(value, DUMMY);
+    }
+
+    @Override
+    public void remove(T value) {
+        map.remove(value);
+    }
+
+    @Override
+    public boolean contains(T value) {
+        return map.containsKey(value);
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return map.keySet().iterator();
+    }
+}
